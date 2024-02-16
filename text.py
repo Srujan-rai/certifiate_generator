62f8b015047afb84f0e54cb12de2f81f4b31c0b5
Srujan-rai <raisrujan@gmail.com>
2024-02-16 14:17:52 +0530

    code push

--- text.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os
-img=cv2.imread('Certificate_of_Participation_G.png')
-
-img=cv2.putText(img,"srujan",(790,855),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,0),3)
-cv2.imshow("image",img)
-cv2.imwrite("image.jpg",img)
-cv2.destroyAllWindows()
