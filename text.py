b589ea099a85e1f4b826b6fb8a67eff6aff5cc8f
Srujan-rai <raisrujan@gmail.com>
2024-06-30 10:53:05 +0530

    certficat testing and alignment

--- text.py
+++ text.py
@@ -1,8 +1,8 @@
 import cv2
 import os
-img=cv2.imread('Certificate v4.png')
+img=cv2.imread('participation.png')
 
-img=cv2.putText(img,"Input text here",(790, 855),cv2.FONT_HERSHEY_COMPLEX,1.3,(0,0,0),3)
+img=cv2.putText(img,"Srujan rai",(100, 595),cv2.FONT_HERSHEY_COMPLEX | cv2.FONT_ITALIC ,1.6,(255,255,255),2,cv2.LINE_AA)
 cv2.imshow("image",img)
 cv2.imwrite("image.jpg",img)
 cv2.destroyAllWindows()
